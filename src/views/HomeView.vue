254c2f3e5ec10ad225ea00f8e487a85d6e144e61
zhende.chen (陳振德) <zhende.chen@gigabyte.com>
2025-02-10 14:18:26 +0800

    crash

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 import { NButton } from 'naive-ui'
+
+document.querySelector("#abc").innerText('abc')
 </script>
 
 <template>
   <main>
+    <div v-for="i in items" :key="i">{{ i }}</div>
     <n-checkbox v-model:checked="value">
       Checkbox
     </n-checkbox>
