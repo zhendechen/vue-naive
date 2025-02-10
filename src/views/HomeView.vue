8eac3b264c8fb73b49ca753580ae2df86d380806
zhende.chen (陳振德) <zhende.chen@gigabyte.com>
2025-02-10 14:00:42 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { NButton } from 'naive-ui'
+</script>
+
+<template>
+  <main>
+    <n-button>
+      Default
+    </n-button>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
